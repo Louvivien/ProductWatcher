b2e11210a479476c0c012175d48839c7ba286fb4
louvivien <vivien.richaud@gmail.com>
2023-06-06 07:53:45 +0200

    undetected chrome

--- app.py
+++ app.py
@@ -58,6 +58,8 @@ def search_stockx(product_name):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-blink-features")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument('--disable-dev-shm-usage')        
+
 
 
     driver.set_page_load_timeout(30)
